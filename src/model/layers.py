b0965412705cd3980840295032f94442cc84737b
Matt Rozanov <matveyrozanov@gmail.com>
2022-09-06 17:05:06 +0300

    Implement Self-Attention layer, AttentionDownBlock and AttentionUpBlock

--- src/model/layers.py
+++ src/model/layers.py
@@ -26,7 +26,6 @@ class TransformerPositionalEmbedding(nn.Module):
 
     def forward(self, timestep):
         # [bs, d_model]
-        print("PE:", self.pe_matrix[timestep].shape)
         return self.pe_matrix[timestep]
 
 
@@ -66,6 +65,7 @@ class UpsampleBlock(nn.Module):
         x = F.interpolate(input_tensor, scale_factor=self.scale, mode="bilinear", align_corners=True)
         x = self.conv(x)
         return x
+
 
 class ConvDownBlock(nn.Module):
     def __init__(self, in_channels, out_channels, num_layers, time_emb_channels, num_groups, downsample=True):
@@ -121,16 +121,185 @@ class ConvUpBlock(nn.Module):
         return x
 
 
+class SelfAttentionBlock(nn.Module):
+    """
+    Self-attention blocks are applied at the 16x16 resolution in the original DDPM paper.
+    Implementation is based on "Attention Is All You Need" paper, Vaswani et al., 2015
+    (https://arxiv.org/pdf/1706.03762.pdf)
+    """
+    def __init__(self, num_heads, in_channels, num_groups=32, embedding_dim=256):
+        super(SelfAttentionBlock, self).__init__()
+        # For each of heads use d_k = d_v = d_model / num_heads
+        self.num_heads = num_heads
+        self.d_model = embedding_dim
+        self.d_keys = embedding_dim // num_heads
+        self.d_values = embedding_dim // num_heads
+
+        self.query_projection = nn.Linear(in_channels, embedding_dim)
+        self.key_projection = nn.Linear(in_channels, embedding_dim)
+        self.value_projection = nn.Linear(in_channels, embedding_dim)
+
+        self.final_projection = nn.Linear(embedding_dim, embedding_dim)
+        self.norm = nn.GroupNorm(num_channels=embedding_dim, num_groups=num_groups)
+
+    def split_features_for_heads(self, tensor):
+        # We receive Q, K and V at shape [batch, h*w, embedding_dim].
+        # This method splits embedding_dim into 'num_heads' features so that
+        # each channel becomes of size embedding_dim / num_heads.
+        # Output shape becomes [batch, num_heads, h*w, embedding_dim/num_heads],
+        # where 'embedding_dim/num_heads' is equal to d_k = d_k = d_v = sizes for
+        # K, Q and V respectively, according to paper.
+        batch, hw, emb_dim = tensor.shape
+        channels_per_head = emb_dim // self.num_heads
+        heads_splitted_tensor = torch.split(tensor, split_size_or_sections=channels_per_head, dim=-1)
+        heads_splitted_tensor = torch.stack(heads_splitted_tensor, 1)
+        return heads_splitted_tensor
+
+    def forward(self, input_tensor):
+        x = input_tensor
+        batch, features, h, w = x.shape
+        # Do reshape and transpose input tensor since we want to process depth feature maps, not spatial maps
+        x = x.view(batch, features, h * w).transpose(1, 2)
+
+        # Get linear projections of K, Q and V according to Fig. 2 in the original Transformer paper
+        queries = self.query_projection(x)  # [b, in_channels, embedding_dim]
+        keys = self.key_projection(x)       # [b, in_channels, embedding_dim]
+        values = self.value_projection(x)   # [b, in_channels, embedding_dim]
+
+        # Split Q, K, V between attention heads to process them simultaneously
+        queries = self.split_features_for_heads(queries)
+        keys = self.split_features_for_heads(keys)
+        values = self.split_features_for_heads(values)
+
+        # Perform Scaled Dot-Product Attention (eq. 1 in the Transformer paper).
+        # Each SDPA block yields tensor of size d_v = embedding_dim/num_heads.
+        scale = self.d_keys ** -0.5
+        attention_scores = torch.softmax(torch.matmul(queries, keys.transpose(-1, -2)) * scale, dim=-1)
+        attention_scores = torch.matmul(attention_scores, values)
+
+        # Permute computed attention scores such that
+        # [batch, num_heads, h*w, embedding_dim] --> [batch, h*w, num_heads, d_v]
+        attention_scores = attention_scores.permute(0, 2, 1, 3).contiguous()
+
+        # Concatenate scores per head into one tensor so that
+        # [batch, h*w, num_heads, d_v] --> [batch, h*w, num_heads*d_v]
+        concatenated_heads_attention_scores = attention_scores.view(batch, h * w, self.d_model)
+
+        # Perform linear projection and reshape tensor such that
+        # [batch, h*w, d_model] --> [batch, d_model, h*w] -> [batch, d_model, h, w]
+        linear_projection = self.final_projection(concatenated_heads_attention_scores)
+        linear_projection = linear_projection.transpose(-1, -2).reshape(batch, self.d_model, h, w)
+
+        # Residual connection + norm
+        x = self.norm(linear_projection + input_tensor)
+        return x
+
+
 class AttentionDownBlock(nn.Module):
-    def __init__(self, **args):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 num_layers,
+                 time_emb_channels,
+                 num_groups,
+                 num_att_heads,
+                 downsample=True):
+        """
+        AttentionDownBlock consists of ResNet blocks with Self-Attention blocks in-between
+        :param in_channels:
+        :param out_channels:
+        :param num_layers:
+        :param time_emb_channels:
+        :param num_groups:
+        :param num_att_heads:
+        :param downsample:
+        """
         super(AttentionDownBlock, self).__init__()
-        raise NotImplementedError
+
+        resnet_blocks = []
+        attention_blocks = []
+        for i in range(num_layers):
+            in_channels = in_channels if i == 0 else out_channels
+            resnet_block = ResNetBlock(in_channels=in_channels,
+                                       out_channels=out_channels,
+                                       time_emb_channels=time_emb_channels,
+                                       num_groups=num_groups)
+            attention_block = SelfAttentionBlock(in_channels=out_channels,
+                                                 embedding_dim=out_channels,
+                                                 num_heads=num_att_heads,
+                                                 num_groups=num_groups)
+
+            resnet_blocks.append(resnet_block)
+            attention_blocks.append(attention_block)
+
+        self.resnet_blocks = nn.ModuleList(resnet_blocks)
+        self.attention_blocks = nn.ModuleList(attention_blocks)
+
+        self.downsample = DownsampleBlock(in_channels=out_channels, out_channels=out_channels, stride=2, padding=1) \
+            if downsample \
+            else None
+
+    def forward(self, input_tensor, time_embedding):
+        x = input_tensor
+        for resnet_block, attention_block in zip(self.resnet_blocks, self.attention_blocks):
+            x = resnet_block(x, time_embedding)
+            x = attention_block(x)
+        if self.downsample:
+            x = self.downsample(x)
+        return x
 
 
-class Bottleneck(nn.Module):
-    def __init__(self, **args):
-        super(Bottleneck, self).__init__()
-        raise NotImplementedError
+class AttentionUpBlock(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 num_layers,
+                 time_emb_channels,
+                 num_groups,
+                 num_att_heads,
+                 upsample=True):
+        """
+        :param in_channels:
+        :param out_channels:
+        :param num_layers:
+        :param time_emb_channels:
+        :param num_groups:
+        :param num_att_heads:
+        :param upsample:
+        """
+        super(AttentionUpBlock, self).__init__()
+
+        resnet_blocks = []
+        attention_blocks = []
+        for i in range(num_layers):
+            in_channels = in_channels if i == 0 else out_channels
+            resnet_block = ResNetBlock(in_channels=in_channels,
+                                       out_channels=out_channels,
+                                       time_emb_channels=time_emb_channels,
+                                       num_groups=num_groups)
+            attention_block = SelfAttentionBlock(in_channels=out_channels,
+                                                 embedding_dim=out_channels,
+                                                 num_heads=num_att_heads,
+                                                 num_groups=num_groups)
+
+            resnet_blocks.append(resnet_block)
+            attention_blocks.append(attention_block)
+
+        self.resnet_blocks = nn.ModuleList(resnet_blocks)
+        self.attention_blocks = nn.ModuleList(attention_blocks)
+
+        self.upsample = UpsampleBlock(in_channels=out_channels, out_channels=out_channels) \
+            if upsample \
+            else None
+
+    def forward(self, input_tensor, time_embedding):
+        x = input_tensor
+        for resnet_block, attention_block in zip(self.resnet_blocks, self.attention_blocks):
+            x = resnet_block(x, time_embedding)
+            x = attention_block(x)
+        if self.upsample:
+            x = self.upsample(x)
+        return x
 
 
 class ResNetBlock(nn.Module):
@@ -163,9 +332,3 @@ class ResNetBlock(nn.Module):
 
         x = self.block2(x)
         return x + self.residual_conv(input_tensor)
-
-
-class SelfAttentionBlock(nn.Module):
-    def __init__(self):
-        super(SelfAttentionBlock, self).__init__()
-        raise NotImplementedError
