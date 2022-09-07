537bda07997b96bd29d3f46f1e091f150a3f7e98
Matt Rozanov <matveyrozanov@gmail.com>
2022-09-07 21:10:19 +0300

    Add images postprocess and grid generating

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,3 +1,6 @@
+from PIL import Image
+
+
 def broadcast(values, broadcast_to):
     values = values.flatten()
 
@@ -5,3 +8,19 @@ def broadcast(values, broadcast_to):
         values = values.unsqueeze(-1)
 
     return values
+
+
+def postprocess(images):
+    images = (images / 2 + 0.5).clamp(0, 1)
+    images = images.cpu().permute(0, 2, 3, 1).numpy()
+    images = (images * 255).round().astype("uint8")
+    return images
+
+
+def create_images_grid(images, rows, cols):
+    images = [Image.fromarray(image) for image in images]
+    w, h = images[0].size
+    grid = Image.new('RGB', size=(cols * w, rows * h))
+    for i, image in enumerate(images):
+        grid.paste(image, box=(i % cols * w, i // cols * h))
+    return grid
