1ce0dd4e53097a07d5bb399cbf03c477722ee138
Matt Rozanov <matveyrozanov@gmail.com>
2022-09-07 21:21:41 +0300

    Parametrized model with initial parameters, remove irrelevant comments

--- src/model/unet.py
+++ src/model/unet.py
@@ -13,7 +13,7 @@ class UNet(nn.Module):
     Model architecture as described in the DDPM paper, Appendix, section B
     """
 
-    def __init__(self):
+    def __init__(self, image_size=256, input_channels=3):
         super().__init__()
         # 1. We replaced weight normalization with group normalization
         # 2. Our 32x32 models use four feature map resolutions (32x32 to 4x4), and our 256x256 models use six (I made 5)
@@ -22,7 +22,7 @@ class UNet(nn.Module):
         # 4. Diffusion time t is specified by adding the Transformer sinusoidal position embedding into
         # each residual block [https://arxiv.org/pdf/1706.03762.pdf]
 
-        self.initial_conv = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, stride=1, padding='same')
+        self.initial_conv = nn.Conv2d(in_channels=input_channels, out_channels=128, kernel_size=3, stride=1, padding='same')
         self.positional_encoding = nn.Sequential(
             TransformerPositionalEmbedding(dimension=128),
             nn.Linear(128, 128 * 4),
@@ -31,11 +31,11 @@ class UNet(nn.Module):
         )
 
         self.downsample_blocks = nn.ModuleList([
-            ConvDownBlock(in_channels=128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128*4),                         # 256x256x128 -> 128x128x128
-            ConvDownBlock(in_channels=128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128*4),                         # 128x128x128 -> 64x64x128
-            ConvDownBlock(in_channels=128, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128*4),                         # 64x64x128 -> 32x32x256
-            AttentionDownBlock(in_channels=256, out_channels=256, num_layers=2, num_att_heads=4, num_groups=32, time_emb_channels=128*4),   # 32x32x256 -> 16x16x256
-            ConvDownBlock(in_channels=256, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128*4)                          # 16x16x256 -> 16x16x256
+            ConvDownBlock(in_channels=128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
+            ConvDownBlock(in_channels=128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
+            ConvDownBlock(in_channels=128, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
+            AttentionDownBlock(in_channels=256, out_channels=256, num_layers=2, num_att_heads=4, num_groups=32, time_emb_channels=128 * 4),
+            ConvDownBlock(in_channels=256, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128 * 4)
         ])
 
         self.bottleneck = AttentionDownBlock(in_channels=256, out_channels=256, num_layers=2, num_att_heads=4, num_groups=32, time_emb_channels=128*4, downsample=False)                                                                                                  # 16x16x256 -> 16x16x256
@@ -43,8 +43,8 @@ class UNet(nn.Module):
         self.upsample_blocks = nn.ModuleList([
             ConvUpBlock(in_channels=256 + 256, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
             AttentionUpBlock(in_channels=256 + 256, out_channels=256, num_layers=2, num_att_heads=4, num_groups=32, time_emb_channels=128 * 4),
-            ConvUpBlock(in_channels=256 + 256, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
-            ConvUpBlock(in_channels=128 + 128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
+            ConvUpBlock(in_channels=256 + 256, out_channels=256, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
+            ConvUpBlock(in_channels=256 + 128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4),
             ConvUpBlock(in_channels=128 + 128, out_channels=128, num_layers=2, num_groups=32, time_emb_channels=128 * 4)
         ])
 
@@ -75,7 +75,7 @@ class UNet(nn.Module):
 
         # Concat initial_conv with tensor
         x = torch.cat([x, states_for_skip_connections[-1]], dim=1)
-        # Get initial shape [3, 226, 226] with convolutions
+        # Get initial shape [3, 256, 256] with convolutions
         out = self.output_conv(x)
 
         return out
